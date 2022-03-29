66ba848c37454f110a6bd01f8d03a47c0b392ee2
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-03-29 20:57:42 +0300

    Add project files.

---
+++ MVCApp/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            return View("Greetings", user);
+        }
+
+    }
+}
